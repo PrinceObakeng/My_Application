546e38b73887a54ac1c97e79c016392dd1f0b2d1
PrinceObakeng <Obakenggaotlhobogwe@gmail.com>
2024-03-07 17:33:14 +0200

    Line 56 indicates an error illegal start of an expression

---
+++ app/src/main/java/p/og/myapplication/MainActivity.java
@@ -0,0 +1,67 @@
+package p.og.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.Priority;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int DEFAULT_UPDATE_INTERVAL = 30;
+    public static final int FAST_UPDATE_INTERVAL = 5;
+
+    // references to the UI elements
+
+TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
+
+   SwitchCompat sw_locationupdates , sw_gps ;
+
+   // variable to remember if we are tracking location or not
+    boolean updateOn = false;
+
+    // The location request is a configuration file containing every setting related to FundLocationProviderClient
+
+    LocationRequest LocationRequest;
+
+// Google's API for location services.The bulk of the app's features use this class.
+   FusedLocationProviderClient fusedLocationProviderClient;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // give each UI variable a value
+
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_altitude = findViewById(R.id.tv_altitude);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_updates = findViewById(R.id.tv_updates);
+        tv_address = findViewById(R.id.tv_address);
+        sw_gps = findViewById(R.id.sw_gps);
+        sw_locationupdates = findViewById(R.id.sw_locationsupdates);
+
+
+
+    // set all properties of Location request
+
+        protected void createLocationRequest() {
+            LocationRequest locationRequest = LocationRequest.Builder()
+                    .setIntervalMillis(10000)
+                    .setFastestIntervalMillis(5000)
+                    .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                    .build();
+        }
+
+
+
+    }
+}
