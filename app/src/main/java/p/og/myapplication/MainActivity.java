40b4bf0a4442e223f2f840ec416d0b5af6aadeed
PrinceObakeng <Obakenggaotlhobogwe@gmail.com>
2024-03-19 16:15:02 +0200

    error: no suitable constructor found for Builder(no arguments)         LocationRequest locationRequest = new LocationRequest.Builder()

--- app/src/main/java/p/og/myapplication/MainActivity.java
+++ app/src/main/java/p/og/myapplication/MainActivity.java
@@ -1,18 +1,29 @@
 package p.og.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
-
-import android.os.Bundle;
-import android.widget.TextView;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.Priority;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class MainActivity extends AppCompatActivity {
     public static final int DEFAULT_UPDATE_INTERVAL = 30;
     public static final int FAST_UPDATE_INTERVAL = 5;
+    private static final int PERMISSIONS_FINE_LOCATION = 95 ;
 
     // references to the UI elements
 
@@ -51,18 +62,86 @@ TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updat
     }
 
 
-
     // set all properties of Location request
 
     private void createLocationRequest() {
-        LocationRequest locationRequest = new LocationRequest.Builder()
-                .setIntervalMillis(10000)
-                .setFastestIntervalMillis(5000)
-                .setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        sw_gps.setOnContextClickListener(new View.OnContextClickListener() {
+            @Override
+            public boolean onContextClick(View v) {
+            if (sw_gps.isChecked()) {
+                //most accurate - use GPS
+                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                tv_sensor.setText("using GPS sensors");
+            }
+            else {
+                locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                tv_sensor.setText("Using Towers + WIFI");
+              }
+                return false;
+            }
+        });
+    } // end onCreate method
+
+
+    
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        
+        switch (requestCode) {
+            case PERMISSIONS_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateGPS();
+                }
+                else  {
+                    Toast.makeText(this,"This app requires permission to be granted for it to work properly" ,Toast.LENGTH_SHORT).show();
+                    finish();
+                    
+                }
+                break;
+                
+        }
+    }
+    private void updateGPS () {
+        //get permission from the user to track GPS
+        //get current location from the fused client
+        //update the UI - i.e set all properties in their associated text view items.
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        Object PackageManager;
+        //Check if the ACCESS_FINE_LOCATION permission is granted
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //user provided permission
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // We got permissions. Put the values of location.XXX into the UI components.
+                updateUIValues (location);
+
+            }
+        });
+
+    }
+    else {
+        //permission is not granted yet.
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},PERMISSIONS_FINE_LOCATION);
+        }
     }
 
 
+}
 
+    private void updateUIValues(Location location) {
 
+        //update all of the text view objects with a new location.
+    }
     }
 
